651b154bab90f2fde53ce335ecb20e18bab8b7e9
Christian <christian.pasin.rey@gmail.com>
2023-06-04 11:43:21 +0200

    Welcome, login and register

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -5,6 +5,7 @@ import InputLabel from '@/Components/InputLabel.vue';
 import PrimaryButton from '@/Components/PrimaryButton.vue';
 import TextInput from '@/Components/TextInput.vue';
 import { Head, Link, useForm } from '@inertiajs/vue3';
+import { ref } from 'vue';
 
 const form = useForm({
     name: '',
@@ -13,7 +14,12 @@ const form = useForm({
     password_confirmation: '',
     terms: false,
 });
-
+const muted = ref(true);
+const video = ref(null);
+const toggleMuteVideo = () => {
+    muted.value = !muted.value;
+    video.value.muted = muted.value;
+}
 const submit = () => {
     form.post(route('register'), {
         onFinish: () => form.reset('password', 'password_confirmation'),
@@ -24,8 +30,28 @@ const submit = () => {
 <template>
     <GuestLayout>
         <Head title="Register" />
-
-        <form @submit.prevent="submit">
+        <div class="absolute top-0 left-0 h-full w-full z-10">
+            <video ref="video" autoplay loop muted class="h-full w-full object-cover">
+                <source src="storage/home-video.mp4" type="video/mp4" />
+            </video>
+        </div>
+        <div class="max-w-7xl mx-auto p-6 lg:p-8 absolute z-20 top-0 left-0 flex">
+            <span @click="toggleMuteVideo" class="rounded-full p-1 border border-slate-100 hover:scale-105 text-white cursor-pointer transition duration-500 ease-in-out">
+                <svg v-if="muted" xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    class="bi bi-volume-mute-fill w-7 h-7" viewBox="0 0 16 16">
+                    <path d="M6.717 3.55A.5.5 0 0 1 7 4v8a.5.5 0 0 1-.812.39L3.825 10.5H1.5A.5.5 0 0 1 1 10V6a.5.5 0 0 1 .5-.5h2.325l2.363-1.89a.5.5 0 0 1 .529-.06zm7.137 2.096a.5.5 0 0 1 0 .708L12.207 8l1.647 1.646a.5.5 0 0 1-.708.708L11.5 8.707l-1.646 1.647a.5.5 0 0 1-.708-.708L10.793 8 9.146 6.354a.5.5 0 1 1 .708-.708L11.5 7.293l1.646-1.647a.5.5 0 0 1 .708 0z"/>
+                </svg>
+                <svg v-else xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    class="bi bi-volume-up w-7 h-7" viewBox="0 0 16 16">
+                    <path d="M11.536 14.01A8.473 8.473 0 0 0 14.026 8a8.473 8.473 0 0 0-2.49-6.01l-.708.707A7.476 7.476 0 0 1 13.025 8c0 2.071-.84 3.946-2.197 5.303l.708.707z"/>
+                    <path d="M10.121 12.596A6.48 6.48 0 0 0 12.025 8a6.48 6.48 0 0 0-1.904-4.596l-.707.707A5.483 5.483 0 0 1 11.025 8a5.483 5.483 0 0 1-1.61 3.89l.706.706z"/>
+                    <path d="M10.025 8a4.486 4.486 0 0 1-1.318 3.182L8 10.475A3.489 3.489 0 0 0 9.025 8c0-.966-.392-1.841-1.025-2.475l.707-.707A4.486 4.486 0 0 1 10.025 8zM7 4a.5.5 0 0 0-.812-.39L3.825 5.5H1.5A.5.5 0 0 0 1 6v4a.5.5 0 0 0 .5.5h2.325l2.363 1.89A.5.5 0 0 0 7 12V4zM4.312 6.39 6 5.04v5.92L4.312 9.61A.5.5 0 0 0 4 9.5H2v-3h2a.5.5 0 0 0 .312-.11z"/>
+                </svg>
+            </span>
+        </div>
+        <form @submit.prevent="submit" class="absolute top-1/3 left-[40%] z-20 bg-slate-200 rounded-md py-4 px-6 opacity-80">
             <div>
                 <InputLabel for="name" value="Nombre" />
 
